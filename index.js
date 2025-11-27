bd1ed6ea54ce8d894e861a2ed4d786bddf71af54
ashique6465 <mdashiqueali71@gmail.com>
2025-11-27 23:02:23 +0530

    Create GET POST PUT UPDATE Method

--- index.js
+++ index.js
@@ -16,6 +16,30 @@ app.get("/users",(req,res)=>{
     res.json(users)
 })
 
+app.post('/users',(req,res)=>{
+    const newUser = {
+        id:users.length + 1 ,
+        name : req.body.name
+    };
+    users.push(newUser);
+    res.json({message: 'User creatd',user:newUser})
+
+})
+
+app.put('/users/:id',(req,res)=>{
+    const id = req.params.id;
+    let user = users.find(u => u.id == id);
+    if(!user) return res.json({message:"user not found"})
+        user.name = req.body.name
+    res.json({message:"User updated",user})
+})  
+
+app.delete('/users/:id',(req,res)=>{
+    const id  = parseInt(req.params.id);
+    users = users.filter(u => u.id != id);
+    res.json({message:"user deleted"});
+})
+
 
 app.listen(3000,"0.0.0.0",() =>{
     console.log("API running")
