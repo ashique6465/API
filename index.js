ce9f14ade144dce2f6f0f7bbaed39a25e8cd20e4
Md Ashique Ali <143936710+ashique6465@users.noreply.github.com>
2025-11-27 16:49:40 +0000

    Create GET API

--- index.js
+++ index.js
@@ -8,4 +8,15 @@ let users = [
     {id:1,name:"john"},
     {id:2,name:"Alice"}
 ];
+app.get("/", (req, res) => {
+    res.send("API is running! Go to /users");
+});
 
+app.get("/users",(req,res)=>{
+    res.json(users)
+})
+
+
+app.listen(3000,"0.0.0.0",() =>{
+    console.log("API running")
+})
